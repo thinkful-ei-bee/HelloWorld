62eafe30abe005ee6803146fa2bea61fb8f3d046
JizongL <2015rpro@gmail.com>
2019-03-20 16:48:34 -0400

    final commit

--- hello-world-app-two/src/App.js
+++ hello-world-app-two/src/App.js
@@ -8,8 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h4>Hello World App Drill</h4>
         <HelloWorld />
+        <h4>Time bomb App Drill</h4>
         
+        <h4>RouletteGun App Drill</h4>
         <RouletteGun bulletInChamber={8}/>
       </div>
     );
---
+++ hello-world-app-two/src/state-drills/RouletteGun2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class RouletteGun2 extends React.Component{
+
+  render(){
+    return(
+
+    )
+  }
+
+}
+
+export default RouletteGun2
