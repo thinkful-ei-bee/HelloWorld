8464e2b12e14338049e0a45a94f794b7e385aaf4
JizongL <2015rpro@gmail.com>
2019-03-20 15:25:17 -0400

    finished Bomb drill

--- hello-world-app-two/src/state-drills/Bomb.js
+++ hello-world-app-two/src/state-drills/Bomb.js
@@ -21,21 +21,32 @@ class Bomb extends React.Component{
 
   componentWillUnmount() {
     clearInterval(this.interval)
+
+
   }
+
+  
+
   render(){
-    if(this.state.count%2===0)
-{return(
-  <div>
-   <p>tick</p>
-  </div>
-)}else{
-  return(
-    <div>
+    let result = this.state.count%2===0?<div>
+    <p>tick</p>
+   </div>:this.state.count>=8?<div>
+      <p>Boom!</p>
+      <img src='https://media.giphy.com/media/3oz8xDp5mAEOAZXEPe/giphy.gif'alt='bomb'/>
+    </div>:
+   <div>
       <p>tock</p>
-    </div>)
+      
+    </div>
+    if(this.state.count>8)clearInterval(this.interval)
+    return(
+     result
+    )
+    
+
 
 }
 }
-}
+
 
 export default Bomb 
